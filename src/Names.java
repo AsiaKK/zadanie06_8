143a742001293fe51b2e849e1a0586fa2327f9ee
AsiaKK <joanna.kaminska83@gmail.com>
2019-01-27 18:55:58 +0100

    first commit

---
+++ src/Names.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Names {
+
+    public static void main(String[] args) {
+
+        String[] allNames = new String[3];
+        allNames[0] = "Asia";
+        allNames[1] = "Basia";
+        allNames[2] = "Kasia";
+
+        System.out.println("Pierwsze imię: " + allNames[0]);
+        System.out.println("Drugie imię: " + allNames[1]);
+        System.out.println("Trzecie imię: " + allNames[2]);
+    }
+}
